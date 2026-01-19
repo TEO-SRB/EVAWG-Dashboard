cbc9e1c329d40b84310c1484275c2cbcd09298c1
andymccannnisra <andrew.mcccann@nisra.gov.uk>
2026-01-19 15:39:32 +0000

    Domestic homicides update

--- src/domestic-homicides.js
+++ src/domestic-homicides.js
@@ -19,15 +19,15 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     updateYearSpans(data, stat);
 
-     insertValue("homicide-box1-female", data.data[stat][latest_year]["All ages"]["Female"]);
-     insertValue("homicide-box1-male", data.data[stat][latest_year]["All ages"]["Male"]);
+     //insertValue("homicide-box1-female", data.data[stat][latest_year]["18+ years"]["Female"]);
+     //insertValue("homicide-box1-male", data.data[stat][latest_year]["18+ years"]["Male"]);
      insertValue("homicide-box2-female", data.data[stat][latest_year]["18+ years"]["Female"]);
      insertValue("homicide-box2-male", data.data[stat][latest_year]["18+ years"]["Male"]);
      insertValue("homicide-box3-girl", data.data[stat][latest_year]["Under 18 years"]["Female"]);
      insertValue("homicide-box3-boy", data.data[stat][latest_year]["Under 18 years"]["Male"]);
 
-    // insertValue("homicide-box1-female", "X");
-    // insertValue("homicide-box1-male", "X");
+    insertValue("homicide-box1-female", "X");
+    insertValue("homicide-box1-male", "X");
     // insertValue("homicide-box2-female", "X");
     // insertValue("homicide-box2-male", "X");
     // insertValue("homicide-box3-girl", "X");
